e9bb80f18140a09aec2a110748c32d4fb604c0a8
Flávia Queiroz <flavia.vgq@gmail.com>
2024-09-09 01:05:02 -0300

    Update settings.py

--- delivery/settings.py
+++ delivery/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p*4jg@m@w51ngh@)72&6orzrvb0e1g^)u!_qztgb9zbi0ygm7o'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
