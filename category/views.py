8acb29a56a1c8a6e774e448415163a91eeee0dc9
FlaviaVGQ <flavia.vgq@gmail.com>
2024-09-25 23:00:39 -0300

    Retorno das categorias

--- category/views.py
+++ category/views.py
@@ -11,13 +11,12 @@ class CategoryView(APIView):
 
     def post(self, request, *args, **kwargs):
         category_name = request.data.get('category')
+        print(category_name)
         user_id = request.data.get('user_id')
 
-        print(category_name)
-        print(user_id)
-
         if not category_name or not user_id:
-            return Response({"error": "Categoria e ID de usuário são obrigatórios."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Categoria e ID de usuário são obrigatórios."},
+                            status=status.HTTP_400_BAD_REQUEST)
 
         try:
             user = User.objects.get(id=user_id)
@@ -29,4 +28,25 @@ class CategoryView(APIView):
         category.save()
 
         return Response({"message": "Categoria criada com sucesso!"}, status=status.HTTP_201_CREATED)
+
+    def get(self, request, *args, **kwargs):
+        user_id = request.query_params.get('user_id')  # Obtém o user_id dos parâmetros da query string
+
+        if not user_id:
+            return Response({"error": "ID de usuário é obrigatório."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({"error": "Usuário não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+
+        # Busca todas as categorias associadas ao usuário
+        categories = Category.objects.filter(user=user)
+        print (categories)
+
+        # Converte as categorias para um formato que pode ser enviado como JSON
+        categories_data = [{"id": category.id, "name": category.name} for category in categories]
+        print(categories_data)
+
+        return Response(categories_data, status=status.HTTP_200_OK)
 
